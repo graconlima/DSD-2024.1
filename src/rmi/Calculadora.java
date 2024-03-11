d8ab479b48e7841d6edfcf1527fbe28e9ca98f0d
graconlima <graconlima@yahoo.com.br>
2024-03-11 11:21:44 -0300

    primeiro commit

--- src/rmi/Calculadora.java
+++
@@ -1,8 +0,0 @@
-package rmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Calculadora extends Remote{
-        public int soma(int a, int b) throws RemoteException;
-}
--- src/rmi2/OlaRMI.java
+++
@@ -1,9 +0,0 @@
-package rmi2;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface OlaRMI extends Remote{
-
-    public String oi() throws RemoteException;
-}
